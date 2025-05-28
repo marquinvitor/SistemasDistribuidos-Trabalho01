8b57f79159f6908f3aaf7372e9ae1a9425078801
marcos vitor <marcosvitorsouzafreire@gmail.com>
2025-05-28 16:11:33 -0300

    Escrever dados terminado

--- SGRH/Control/colaboradorOutputStream.go
+++ SGRH/Control/colaboradorOutputStream.go
@@ -1,31 +1,56 @@
 package Control
 
-import(
+import (
+	"fmt"
 	"io"
 	"meu_rh/Models"
-	"fmt"
 )
 
-type ColaboradorOuputStream struct{
-	Destino io.Writer
+type ColaboradorOuputStream struct {
+	Destino       io.Writer
 	Colaboradores []Models.Colaborador
 }
 
-func NewColaboradorOutputStream(destino io.Writer,dados []Models.Colaborador) *ColaboradorOuputStream{
+func NewColaboradorOutputStream(destino io.Writer, dados []Models.Colaborador) *ColaboradorOuputStream {
 	return &ColaboradorOuputStream{
-		Destino: destino,
+		Destino:       destino,
 		Colaboradores: dados,
 	}
 }
 
-func(cos *ColaboradorOuputStream)EscreverTodosOsDados()error{
+func (cos *ColaboradorOuputStream) EscreverTodosOsDados() error {
 	numObjetos := len(cos.Colaboradores)
 	cabecalho := fmt.Sprintf("Enviando dados de %d objetos...\n---\n", numObjetos)
 	_, err := cos.Destino.Write([]byte(cabecalho))
 
-	if err != nil{
-		return fmt.Errorf("falha ao escrever cabecalho: %w",err)
+	if err != nil {
+		return fmt.Errorf("falha ao escrever cabecalho: %w", err)
 	}
 
-	//inacabado
+	for _, colab := range cos.Colaboradores {
+		idStr := colab.GetId()
+		nome := colab.GetNome()
+		salarioStr := fmt.Sprintf("%.2f", colab.CalcularSalario())
+
+		linha := fmt.Sprintf("ID: %s, Nome: %s Salario: %s", idStr, nome, salarioStr)
+
+		dadosBytes := []byte(linha)
+
+		bytesEscritos, err := cos.Destino.Write(dadosBytes)
+
+		if err != nil {
+			return fmt.Errorf("falha ao escrever os dados: %w", err)
+		}
+
+		fmt.Printf("Foram escritos %d bytes", bytesEscritos)
+
+		final := "---\nEnvio concluído.\n"
+		_, err = cos.Destino.Write([]byte(final))
+
+		if err != nil {
+			return fmt.Errorf("falha ao escrever finalização: %w", err)
+		}
+
+	}
+	return nil
 }
